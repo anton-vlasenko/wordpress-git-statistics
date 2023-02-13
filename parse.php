66dd6c020b4cb615cd662c387d617477240b607d
Anton Vlasenko <vlasenko.anton@gmail.com>
2023-02-13 15:21:32 +0100

    Fix file name.

--- parse.php
+++ parse.php
@@ -1,6 +1,6 @@
 <?php
 const DS = DIRECTORY_SEPARATOR;
-require_once __DIR__ . DS . 'boostrap.php';
+require_once __DIR__ . DS . 'bootstrap.php';
 
 if ( empty( $developers ) || empty( $repositories ) ) {
 	throw new RuntimeException( 'config.php is invalid.' );
